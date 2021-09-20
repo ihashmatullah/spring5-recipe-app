29e9e5fab55929652db69d8c62510151c7616ba3
ihashmatullah <hashnoorzai@yahoo.com>
2021-09-20 11:36:31 -0500

    Completing assignment

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -0,0 +1,62 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by HashmatN at 10:18 AM on Sep 20, 2021
+ */
+@Entity
+public class Ingredient {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String description;
+  private String amount;
+
+  @OneToOne(fetch = FetchType.EAGER)
+  private UnitOfMeasure uom;
+
+  @ManyToOne
+  private Recipe recipe;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getAmount() {
+    return amount;
+  }
+
+  public void setAmount(String amount) {
+    this.amount = amount;
+  }
+
+  public Recipe getRecipe() {
+    return recipe;
+  }
+
+  public void setRecipe(Recipe recipe) {
+    this.recipe = recipe;
+  }
+
+  public UnitOfMeasure getUom() {
+    return uom;
+  }
+
+  public void setUom(UnitOfMeasure uom) {
+    this.uom = uom;
+  }
+}
