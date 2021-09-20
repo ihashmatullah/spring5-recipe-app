673e0b51b4795d99299577eb1d41e2dfc8c9ae0c
ihashmatullah <hashnoorzai@yahoo.com>
2021-09-20 12:17:35 -0500

    Sec8 Lect138 Many to Many JPA relationships

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring5recipeapp.domain;
+
+/**
+ * Created by HashmatN at 11:39 AM on Sep 20, 2021
+ */
+public enum Difficulty {
+  EASY, MODERATE, KIND_OF_HARD, HARD
+}
